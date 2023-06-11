083b94df406a8d699122aac7de971907cf269d78
GradeEscolar <gerson.msj2@gmail.com>
2023-06-11 15:32:29 -0300

    Deploy

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/grade_escolar_web'
+        ? '/web'
         : '/',
     outputDir: 'docs',
     configureWebpack: {
